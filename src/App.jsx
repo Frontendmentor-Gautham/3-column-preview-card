fc8156a9f0490290478261fcafa9720eb0d0227f
Gautham Vanjre <vanjregautham@gmail.com>
2022-10-30 12:27:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainComponent from "./MainComponent";
+
+export default () => (
+  <>
+    <MainComponent />
+  </>
+);
---
+++ src/MainComponent.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/style.css";
+
+const MainComponent = () => {
+  return (
+    <>
+      <main>
+        <div>
+          <img src="./images/icon-sedans.svg" alt="sedan icon" />
+          <h3>SEDANS</h3>
+          <p>
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </p>
+          <button className="btn">Learn More</button>
+        </div>
+
+        <div>
+          <img src="./images/icon-suvs.svg" alt="suv icon" />
+          <h3>SUVs</h3>
+          <p>
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </p>
+          <button className="btn">Learn More</button>
+        </div>
+
+        <div>
+          <img src="./images/icon-luxury.svg" alt="luxury icon" />
+          <h3>LUXURY</h3>
+          <p>
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </p>
+          <button className="btn">Learn More</button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default MainComponent;
